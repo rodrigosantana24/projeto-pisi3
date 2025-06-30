413e1e620a74d08879ef9928481dad5836c80234
Gabriel Vinícius <gabrielvto18@gmail.com>
2025-06-30 17:45:00 -0300

    removendo arquivos inutilizados

--- Streamlit/src/navigation.py
+++
@@ -1,8 +0,0 @@
-def show_navigation():
-    import streamlit as st
-
-    st.sidebar.title("Navigation")
-    options = ["Exploratory Analysis", "Classification", "Clustering"]
-    choice = st.sidebar.radio("Select a tab:", options)
-
-    return choice
